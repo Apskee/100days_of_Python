55221e89d456a2940b48aa6528b6ea8bbc99e4d5
Miloš Ragulík <ragulik.milos@gmail.com>
2022-12-21 08:50:59 +0100

    Adding day 1 final project of 100 days Python challenge

---
+++ D1_band_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band generator!!")
+city = input("What is the city you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+print (city + " " + pet)
---
+++ D1_variable switching.py
@@ -0,0 +1,18 @@
+#asking for input
+n1 = input("number1: ")
+n2 = input("number2: ")
+print(n1)
+print(n2)
+
+######## SOLUTION #######
+
+#n1 = 10, n2=20
+temp = n1
+n1 = n2
+n2 = temp
+
+
+
+#printing outcome
+print(n1)
+print(n2)
